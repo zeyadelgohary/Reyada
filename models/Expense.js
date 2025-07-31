75f45acb731d90d7c4ee295c78af1a40fdc72df6
zeyad <zeyadelgohary217@gmail.com>
2025-07-31 15:34:26 +0300

    modify Expense Model

--- models/Expense.js
+++ models/Expense.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const ExpenseSchema = new mongoose.Schema({
   date: { type: Date, required: true },
-  period: { type: String, enum: ["daily", "monthly", "yearly"], required: true },
+  period: { type: String, enum: ["daily", "monthly", "yearly"]},
   name: { type: String, required: true }, 
   amount: { type: Number, required: true }, 
-  total: { type: Number, required: true }
+  total: { type: Number }
 });
 
 module.exports = mongoose.model("Expense", ExpenseSchema);
